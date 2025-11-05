cc56c3849fb3ee366c8de97a1dc6a76221f950ff
Ulysse Ballesteros <ulysseballesteros@gmail.com>
2025-11-05 10:24:19 +0100

    Refactor device definitions and configurations

--- sg230r2/bootblock.c
+++ sg230r2/bootblock.c
@@ -12,7 +12,10 @@
 #include "ezio_lcd.h"
 
 #define GLOBAL_DEV PNP_DEV(0x2E, 0)
-#define UART1_DEV PNP_DEV(0x2E, NCT6779D_SP1)
+#define SERIAL_DEV PNP_DEV(0x2E, NCT6779D_SP1)
+#define ACPI_DEV   PNP_DEV(0x2E, NCT6779D_ACPI)
+#define HWM_DEV    PNP_DEV(0x2E, NCT6779D_HWM_FPLED)
+#define GPIO_DEV   PNP_DEV(0x2E, NCT6779D_GPIO_PP_OD)
 /*
  * early_config_superio - Configure SuperIO NCT6779D chip
  *
@@ -51,10 +54,76 @@ static void early_config_superio(void)
 	pnp_write_config(GLOBAL_DEV, 0x2C, 0x00);
 	pnp_write_config(GLOBAL_DEV, 0x2F, 0x00);
 
+	/* === LDN 0x07 (GPIO6/7/8) === */
+    pnp_set_logical_device(PNP_DEV(0x2E, NCT6779D_GPIO678_V));
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO678_V), 0xE0, 0x73);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO678_V), 0xE1, 0x04);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO678_V), 0xE5, 0xFF);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO678_V), 0xE6, 0xFF);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO678_V), 0xED, 0xFF);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO678_V), 0xF4, 0x00);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO678_V), 0xF5, 0xFF);
+
+    /* === LDN 0x08 (WDT1, GPIO0/1) === */
+    pnp_set_logical_device(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V));
+	pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0x30, 0x0A);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0x60, 0x0A);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0x61, 0x00);
+
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0xE0, 0x1C);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0xE1, 0x0C);
+	
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0xF1, 0xFF);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0xF2, 0xFF);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0xF4, 0xFF);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0xF5, 0xFF);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_WDT1_GPIO01_V), 0xF7, 0xFF);
+
+    /* === LDN 0x09 (GPIO1-8) === */
+    pnp_set_logical_device(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V));
+
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0x30, 0xDC);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0xE0, 0xCF);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0xE1, 0xE0);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0xE4, 0x0F);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0xE5, 0x06);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0xEB, 0xFF);
+	
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0xF0, 0xE7);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0xF1, 0x7A);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0xF5, 0xFF);
+    pnp_write_config(PNP_DEV(0x2E, NCT6779D_GPIO12345678_V), 0xF6, 0xFF);
+
+
+	/* === LDN 0x0B (HWM + Front Panel LED) === */
+    pnp_set_logical_device(HWM_DEV);
+    pnp_write_config(HWM_DEV, 0x60, 0x0A);
+    pnp_write_config(HWM_DEV, 0x61, 0x30);
+    pnp_write_config(HWM_DEV, 0x62, 0x0A);
+    pnp_write_config(HWM_DEV, 0x63, 0x40);
+    pnp_write_config(HWM_DEV, 0x70, 0x00);
+    pnp_write_config(HWM_DEV, 0xE0, 0x7F);
+    pnp_write_config(HWM_DEV, 0xE1, 0x7F);
+	
+    pnp_write_config(HWM_DEV, 0xE2, 0x7F);
+    pnp_write_config(HWM_DEV, 0xE4, 0x7F);
+    pnp_write_config(HWM_DEV, 0xF0, 0x00);
+    pnp_write_config(HWM_DEV, 0xF1, 0x00);
+    pnp_write_config(HWM_DEV, 0xF2, 0x00);
+    pnp_write_config(HWM_DEV, 0xF5, 0x00);
+    pnp_write_config(HWM_DEV, 0xF6, 0x00);
+    pnp_write_config(HWM_DEV, 0xF7, 0x87);
+	
+    pnp_write_config(HWM_DEV, 0xF8, 0x47);
+    pnp_write_config(HWM_DEV, 0xF9, 0x00);
+    pnp_write_config(HWM_DEV, 0xFA, 0x00);
+    pnp_write_config(HWM_DEV, 0xFB, 0x00);
+    pnp_set_enable(HWM_DEV, 1);
+
 	nuvoton_pnp_exit_conf_state(GLOBAL_DEV);
 	
 	/* Configure UART1 for coreboot debug console */
-	nuvoton_enable_serial(UART1_DEV, CONFIG_TTYS0_BASE);
+	nuvoton_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
 }
 
 /*
